942147f56a7b3f6a0aedd062d11f145a6eb0e878
Might Lin <might0215+github@gmail.com>
2022-10-20 15:40:09 +0800

    init

---
+++ projects/ngx-animates/src/public-api.ts
@@ -0,0 +1,85 @@
+import {
+  AnimationReferenceMetadata,
+  animation,
+  style,
+  animate,
+  keyframes,
+} from '@angular/animations';
+
+export {
+  bounce,
+  flash,
+  pulse,
+  rubberBand,
+  shake,
+  swing,
+  tada,
+  wobble,
+  jello,
+} from './lib/attention-seekers';
+
+export {
+  bounceIn,
+  bounceInDown,
+  bounceInLeft,
+  bounceInRight,
+  bounceInUp,
+  bounceOut,
+  bounceOutDown,
+  bounceOutLeft,
+  bounceOutRight,
+  bounceOutUp,
+} from './lib/bouncing';
+
+export {
+  fadeIn,
+  fadeInDown,
+  fadeInUp,
+  fadeInLeft,
+  fadeInRight,
+  fadeOut,
+  fadeOutDown,
+  fadeOutUp,
+  fadeOutLeft,
+  fadeOutRight,
+  slideInDown,
+  slideInUp,
+  slideInLeft,
+  slideInRight,
+  slideOutDown,
+  slideOutUp,
+  slideOutLeft,
+  slideOutRight,
+} from './lib/fading';
+
+export { flip, flipInX, flipInY, flipOutX, flipOutY } from './lib/flippers';
+
+export { lightSpeedIn, lightSpeedOut } from './lib/lightspeed';
+
+export {
+  rotateIn,
+  rotateInDownLeft,
+  rotateInDownRight,
+  rotateInUpLeft,
+  rotateInUpRight,
+  rotateOut,
+  rotateOutDownLeft,
+  rotateOutDownRight,
+  rotateOutUpLeft,
+  rotateOutUpRight,
+} from './lib/rotate';
+
+export { hinge, jackInTheBox, rollIn, rollOut } from './lib/specials';
+
+export {
+  zoomIn,
+  zoomInDown,
+  zoomInUp,
+  zoomInLeft,
+  zoomInRight,
+  zoomOut,
+  zoomOutDown,
+  zoomOutUp,
+  zoomOutLeft,
+  zoomOutRight,
+} from './lib/zooming';
